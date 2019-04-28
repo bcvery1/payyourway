c4d4f72e944948057395bcf19ec04cc2c18b4641
Ben Cragg <bcvery1@gmail.com>
2019-04-28 22:13:48 +0100

    Handling fourth shop

--- shop.go
+++ shop.go
@@ -238,6 +238,18 @@ func (s *Shop) Setup(shopName string) {
 		s.AddItem(99, "Max HP Boost", "Permanently adds\n20 extra HP")
 		s.AddItem(99, "Max HP Boost", "Permanently adds\n20 extra HP")
 		s.AddItem(200, "Invincibility", "Provides 10\nseconds of\ninvincibility")
+	case "FourthLevelMid":
+		s.nextLevel = EndInd
+
+		s.AddItem(45, "Light Shield", "Provides 20\npoints of\nprotection")
+		s.AddItem(80, "Shield", "Provides 40\npoints of\nprotection")
+		s.AddItem(80, "Shield", "Provides 40\npoints of\nprotection")
+		s.AddItem(120, "Heavy Shield", "Provides 60\npoints of\nprotection")
+		s.AddItem(99, "Max HP Boost", "Permanently adds\n20 extra HP")
+		s.AddItem(150, "Flares", "One use deploy\nflares")
+		s.AddItem(200, "Invincibility", "Provides 10\nseconds of\ninvincibility")
+		s.AddItem(200, "Invincibility", "Provides 10\nseconds of\ninvincibility")
+		s.AddItem(250, "Super Jump", "Let's you jump\ntwice as high")
 	default:
 		panic("Unrecognised shop name " + shopName)
 	}
